f0ef2f1514be501cde9c0db5b1cb487de145c13e
tang3w <tang3w@gmail.com>
2013-04-15 13:44:39 +0800

    Add subpod update command

--- lib/subpod.rb
+++ lib/subpod.rb
@@ -7,19 +7,19 @@ module SubPod
   module Delegate
     module_function
 
-    def install
+    def install(update_mode = false)
       cfg = Pod::Config.instance
       cfg.podfile or fail Pod::Informative, "No `Podfile` found in the current working directory."
       installer = Installer.new(cfg.sandbox, cfg.podfile, cfg.lockfile)
-      installer.update_mode = false
+      installer.update_mode = update_mode
       installer.install!
     end
   end
 
   module_function
 
-  def run cmd
-    Delegate.send cmd
+  def run(cmd, *args, &block)
+    Delegate.send(cmd, *args, &block)
   rescue StandardError => e
     puts e.message
   rescue Exception => e
